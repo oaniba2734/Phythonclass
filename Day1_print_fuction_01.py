afd909f5e9ada2ffd2c51f4db41d2688529a0360
oaniba2734 <tomipraise11@gmail.com>
2026-01-03 17:35:12 -0500

    Create Day1_print_fuction_01.py

---
+++ Day1_print_fuction_01.py
@@ -0,0 +1,12 @@
+print("Hello, Tomi")
+print("How are you doing?")
+print("Angie Respond: Hey,Tomi is a lazy boy")
+print("Angie Respond: Well, he's my Big Bro.... So dont mess with him.")
+
+print("""
+Hello, Tomi
+How are you doing?
+Angie Respond: Hey Tomi, mum is calling you right now.
+""")
+
+print("Hello, Tomi \nHow are you doing? \nAngie Respond: Hey Tomi, Mum is calling you right now.")
